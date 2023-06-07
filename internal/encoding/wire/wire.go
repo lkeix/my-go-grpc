f7639a1fd6a1caa2a0f9740ed0dcf045eb95a385
lkeix <ai.kanaria.ai@gmail.com>
2023-06-07 23:51:51 +0900

    add: Valid Variable

--- internal/encoding/wire/wire.go
+++ internal/encoding/wire/wire.go
@@ -1,1 +1,19 @@
 package wire
+
+type Number int32
+
+const (
+	MinValidNumber      Number = 1
+	FirstReservedNumber Number = 19000
+	LastReservedNumber  Number = 19999
+	MaxValidNumber      Number = 1<<29 - 1
+)
+
+func (n Number) IsValid() bool {
+	return MinValidNumber <= n && n < FirstReservedNumber ||
+		LastReservedNumber < n && n <= MaxValidNumber
+}
+
+type Type int8
+
+const ()
